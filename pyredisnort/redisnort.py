2c26dfb244651a5eaaca6cc41d84ba14c15fd915
Jeff Rabinowitz <rabinowitzjeffc@gmail.com>
2014-03-20 15:15:39 -0400

    stubbed out modules for interacting with redis

---
+++ pyredisnort/redisnort.py
@@ -0,0 +1,19 @@
+"""Module redisnort
+
+Perform algorithmic analysis on snort's logged web traffic and save to redis.
+
+The tentative purpose of this module is to serve as an
+algorithmic analyzer on web traffic. The traffic should be parsed from
+log files using conventions established in the snortlogger module and 
+it should then be committed to redis using the function in the
+redislogger module.
+"""
+
+
+
+def main():
+    # do stuff
+    pass
+
+if __name__ == "__main__":
+    main()
